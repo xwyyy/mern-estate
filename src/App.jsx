c9b43f0e93e9a88fdc1d751e9edc2b6eaf19ce68
Olivia-Xiao11 <olivio>
2024-09-12 20:34:40 +0800

    create pages and routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
-import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import SignIn from './pages/SignIn';
+import Profile from './pages/Profile';
+import SignUp from './pages/SignUp';
 
 export default function App() {
   return (
-    <h1 className='text-red-300'>App</h1>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/sign-in' element={<SignIn />} />
+        <Route path='/sign-up' element={<SignUp />} />
+        <Route path='/profile' element={<Profile />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
---
+++ src/pages/Profile.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Profilo() {
+  return (
+    <div>Profilo</div>
+  )
+}

ad3c702ad3a874196b55a18a8464e1114ada284e
xwyyy <xiaow21@mcmaster.ca>
2026-05-29 14:44:14 -0400

    Configure port using environment variable

--- api/index.js
+++ api/index.js
@@ -17,7 +17,7 @@ mongoose
     console.log(err);
   });
 
-  const __dirname = path.resolve();
+const __dirname = path.resolve();
 
 const app = express();
 
@@ -25,8 +25,9 @@ app.use(express.json());
 
 app.use(cookieParser());
 
-app.listen(3000, () => {
-  console.log('Server is running on port 3000!');
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
 
 app.use('/api/user', userRouter);

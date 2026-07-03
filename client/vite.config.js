b9713001659c13b4a89fdb0b3d432888e5fb695a
xwyyy <xiaow21@mcmaster.ca>
2026-07-02 23:52:22 -0400

    feat: add frontend and backend Docker containers

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,9 +4,13 @@ import react from '@vitejs/plugin-react-swc';
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
+    host: true,
+    watch: {
+      usePolling: true,
+    },
     proxy: {
       '/api': {
-        target: 'http://localhost:3000',
+        target: 'http://api:3000',
         secure: false,
       },
     },
